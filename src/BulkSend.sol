9777e135e0e7cf53d071fa46a657b0c966d0e521
Salami Khalil <salamikhalil02@gmail.com>
2023-12-03 21:15:34 +0100

    corrected import error

--- src/BulkSend.sol
+++ src/BulkSend.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.22;
 
-import "./IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 /**
  * @title BulkSend Contract
