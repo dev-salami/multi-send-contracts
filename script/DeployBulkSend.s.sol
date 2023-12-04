5f5bb64ef27632f2677218264a101bc3f6d91bc4
Salami Khalil <salamikhalil02@gmail.com>
2023-12-04 10:37:59 +0100

    bulksend contract deploy script

---
+++ script/DeployBulkSend.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+import {Script} from "forge-std/Script.sol";
+import {BulkSend} from "../src/BulkSend.sol";
+
+contract DeployBulkSend is Script {
+    function run() external returns (BulkSend) {
+        vm.startBroadcast();
+        BulkSend bulksend = new BulkSend();
+        vm.stopBroadcast();
+        return bulksend;
+    }
+}
